90374da8de9447986de7cb3765276b438ab7eac2
WDanielLiu <wliu@rxhui.com>
2017-01-22 11:32:58 +0800

    test react

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-    phpinfo();
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="./js/react.js"></script>
+        <script src="./js/react-dom.js"></script>
+    </head>
+    <body>
+        <div id="example"></div>
+        <script type="text/babel">
+            ReactDom.render(
+              <h1>Hello,world!</h1>
+                document.getElementById('example');
+            );
+        </script>
+    </body>
+</html>
